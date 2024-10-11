020b50f6599f3f96ad9c34548cd1c2eb5d6885f0
Bigsmooth68 <olivier.spiesser@gmail.com>
2024-10-11 09:39:36 +0200

    work prep for url compare

--- node_exporter_optimizer.py
+++ node_exporter_optimizer.py
@@ -1,21 +1,38 @@
 import argparse, requests, re
+import sys, logging
+
+def format_url(url: str):
+    return url if args.url.startswith('http') else 'http://' + url + '/metrics'
+
 
 if __name__ == '__main__':
+
+    # Prepare logger
+    logger = logging.getLogger("node_exporter_optimizer")
+    logging.basicConfig(stream=sys.stdout)
+
     # Argument management
     parser = argparse.ArgumentParser(
                         prog='node_exporter_optimizer',
                         description='Simple script to optimize node manager startup options based on exposed metrics')
-    parser.add_argument('--url', type=str, default='http://localhost:9100')
+    parser.add_argument('--url', type=str, default='http://localhost:9100', help='url of node_exporter to analyze')
+    parser.add_argument('--compare', '-c', type=str, nargs=2, help='provide two node_exporter url to compare list of metrics')
+    parser.add_argument('--debug', action='store_true', help='More verbose output')
     args = parser.parse_args()
 
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
     # Add http if missing
-    url = args.url if args.url.startswith('http') else 'http://' + args.url + '/metrics'
+    url = format_url(args.url)
 
     # Gathering metrics
     try:
+        logger.debug(f"Trying to connect to {url}")
         metrics = requests.get(url).text
+        logger.debug("Connected")
     except:
-        print(f'Cannot connect to {url}')
+        logger.error(f'Cannot connect to {url}')
         exit(1)
 
     count, node_exporter_arguments = 0, ''
@@ -24,7 +41,8 @@ if __name__ == '__main__':
         if line.startswith('node_scrape_collector_success'):
             result = re.findall('"(.*)"', line)
             collector = result[0]
-            if ' 0' in line:   
+            if ' 0' in line:
+                logger.debug(f"Failed collector found: {collector}")
                 node_exporter_arguments += f'--no-collector.{collector} '
                 count += 1
     
